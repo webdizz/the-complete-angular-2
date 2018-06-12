133a7630efdfceb7b47da84cca980cb1b97d779d
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-06-13 00:30:02 +0300

    Add intercomponent communication

--- tries/src/app/app.component.ts
+++ tries/src/app/app.component.ts
@@ -12,9 +12,25 @@ export class AppComponent {
   log = []
 
   serverElements = [{ type: 'server', name: 'TestServer', content: 'Just a test!' }]
-  
+
   onToggleDetails() {
     this.showSecret = !this.showSecret
     this.log.push(new Date())
+  }
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+
+  onBluePrintAdded(bluePrintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    })
   }
 }
--- tries/src/app/cockpit/cockpit.component.ts
+++ tries/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  @Output()
+  serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>;
+
+  @Output()
+  bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>;
+
+  newServerName = ''
+  newServerContent = ''
+
   constructor() { }
+
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
+
+  onAddBluePrint() {
+    this.bluePrintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
 
   ngOnInit() {
   }
